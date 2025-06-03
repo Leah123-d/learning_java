9ce9da7e7cfca0786f0f5157ad821783fa54a923
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-06-03 16:41:12 -0700

    adds variables

--- java1/Introductions.java
+++ java1/Introductions.java
@@ -5,8 +5,8 @@ public class Introductions {
     public static void main(String[] args) {
         Console console = System.console();
         // Welcome to the Introductions program!  Your code goes below here
-      console.printf("hello, my name is Leah\n"); //using a new line escape sequence are used to write characters that may break the syntax of the code 
-      console.printf("Leah, is learning to write Java\n");
-        
+      String firstName = console.readLine("What is your name? "); 
+      console.printf("hello, my name is %s\n", firstName);
+      console.printf("%s, is learning to write Java\n", firstName);
   }
 }
